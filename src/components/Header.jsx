fc768801dbd46314b31fe3619e79e0af475d296c
stereaam <mihai.sterea@yahoo.com>
2021-12-30 17:48:24 +0200

    modified header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,20 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import Logo from '../assets/logo.png'
+import './Header.css'
 
 
 function Header() {
     return (
-        <div>
-            <Link to='/'>Home</Link>
-            <Link to='/login'>Login</Link>
-        </div>
+        <header className='header'>
+            <div className='container-fluid container-min-max-width d-flex justify-content-between align-items-center'>
+                
+                <Link to='/'><img src={Logo} alt="Laguna Shop" className='logo'/></Link>
+                <div>
+                    <Link to='/login'><h2>Login</h2></Link>
+                </div>
+            </div>
+        </header>
     )
 }
 
