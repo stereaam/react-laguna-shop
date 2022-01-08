528fe342ee431224433fe0c10cc556a9b98be443
stereaam <mihai.sterea@yahoo.com>
2022-01-08 19:20:16 +0200

    updated header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,20 +2,29 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import Logo from '../assets/logo.png'
 import './Header.css'
+import { connect } from 'react-redux'
+import { ReactComponent as ShoppingCart } from '../assets/icons/shopping-cart.svg';
 
 
-function Header() {
+function Header(props) {
+    const {numberOfProducts} = props
     return (
         <header className='header'>
             <div className='container-fluid container-min-max-width d-flex justify-content-between align-items-center'>
                 
                 <Link to='/'><img src={Logo} alt="Laguna Shop" className='logo'/></Link>
-                <div>
-                    <Link to='/login'><h2>Login</h2></Link>
+                <div className='d-flex align-items-center'>
+                    <Link to='/login'><h2 className='mx-1'>Login</h2></Link>
+                    <Link to='/cart'><ShoppingCart className='mx-2'/></Link>
+                    <p className='my-2 mr-1 number-of-products text-white'>{numberOfProducts}</p>
                 </div>
             </div>
         </header>
     )
 }
-
-export default Header
+function mapStateToProps(state){
+    return {
+        numberOfProducts: state.products.length
+    }
+}
+export default connect(mapStateToProps)(Header)
