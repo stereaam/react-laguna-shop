d946157c19a0a4ba7a2e4b6fa1f3c0e512e98d03
stereaam <mihai.sterea@yahoo.com>
2022-01-09 18:22:30 +0200

    updated login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,15 +1,50 @@
 import React from 'react'
-import Layout from '../components/Layout'
+import {Link} from 'react-router-dom'
+import {ReactComponent as Google} from '../assets/icons/google.svg'
+import {ReactComponent as HomeIcon} from '../assets/icons/home.svg'
+import './Login.css'
+import {connect} from 'react-redux'
+import {loginUser} from '../redux/actions/user'
 
+class Login extends React.Component {
 
-function Login() {
-    return (
-        <div>
-            <Layout>
+    componentDidUpdate(prevProps){
+        if( this.props.userData !== prevProps.userData){
+            this.props.history.push('/')
+        }
+    }
+    
+    render(){
+        const {signInWithGoogle} = this.props
+        return (
+            <div className="login-page">
+                <Link to='/'>
+                    <button className="hover-zoom btn btn-outline-light d-flex align-items-center mb-5">
+                        <HomeIcon/>
+                        <span className="m-3">Take me back Home</span>
+                    </button>
+                </Link>
                 <h1>Login</h1>
-            </Layout>
-        </div>
-    )
+                <p><em>Please choose your provider below:</em></p>
+                <button className="hover-zoom btn btn-outline-light d-flex align-items-center" onClick={signInWithGoogle}>
+                    <Google/>
+                    <span className="text-nowrap m-3">Login with Google</span>
+                </button>   
+            </div>
+        )
+    }
 }
 
-export default Login
+const mapStateToProps = (state) => {
+    return {
+        userData : state.user.data
+    }
+}
+
+const mapDispatchToProps = (dispatch) =>{
+    return {
+        signInWithGoogle: () => dispatch(loginUser())
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
