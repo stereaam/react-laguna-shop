b96da1eeadc95658ded131fa79b6eea2f061f1a6
stereaam <mihai.sterea@yahoo.com>
2022-01-09 18:25:41 +0200

    created about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import Layout from '../components/Layout'
+import './About.css'
 
 function About() {
     return (
-        <div>
+        <div className='about-page '>
             <Layout>
-                <h1>About</h1>    
+                <div className='container-fluid container-min-max-width
+                d-flex flex-column justify-content-center align-items-center'>
+                    <h1 className='text-center text-white py-2 mt-3 about-us'>About Us</h1>   
+                    <p className='text-center px-5 py-2 text-white'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aperiam suscipit est accusantium, dignissimos natus voluptatum unde reprehenderit. Sunt impedit minus quibusdam excepturi modi, velit ea tempora non iusto, iure at numquam a! Soluta numquam nisi fugit, perspiciatis vitae aut voluptates molestias modi. Ex!  Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus maiores ut fugiat repudiandae ullam aut ea, cumque qui, debitis, cupiditate placeat praesentium numquam doloremque distinctio sapiente voluptate accusantium quasi iusto eum delectus! Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, aspernatur dolorum possimus eveniet quasi harum. Illo deserunt accusantium veritatis ducimus vel minus harum, assumenda officia tempora a ea ullam inventore quas laudantium earum. Aspernatur obcaecati tenetur perspiciatis cumque hic ipsam, enim doloribus aliquam, incidunt dolore similique!</p>
+                </div>
+               
             </Layout>
         </div>
     )
