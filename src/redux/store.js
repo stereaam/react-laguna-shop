c818d4cb2d0cbcf5807c639e50485888642237e1
stereaam <mihai.sterea@yahoo.com>
2022-01-09 18:18:14 +0200

    install redux-thunk and update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,13 @@
-import { createStore } from "redux";
+import { createStore , combineReducers, applyMiddleware } from "redux";
 import cartReducer from './reducers/cart'
+import userReducer from "./reducers/user";
+import thunk from 'redux-thunk'
 
-const store = createStore(cartReducer)
+const rootReducer = combineReducers({
+    cart: cartReducer,
+    user: userReducer}
+    )
+
+const store = createStore(rootReducer, applyMiddleware(thunk))
 
 export default store
