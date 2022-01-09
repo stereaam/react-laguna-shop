3828888ae2a81e0224af320da74780466252682a
stereaam <mihai.sterea@yahoo.com>
2022-01-09 18:20:39 +0200

    updated layout component

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import Header from './Header'
 import Footer from './Footer'
+import './Layout.css'
 
 
 function Layout(props) {
     return (
-        <div>
+        <div className='layout'>
             <Header/>
                 {props.children}
             <Footer/>
