e5e1c678eea35f400d5ee3ce2728464757433652
stereaam <mihai.sterea@yahoo.com>
2022-01-16 18:45:06 +0200

    updated Cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,12 +3,20 @@ import Layout from '../components/Layout'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {ReactComponent as HomeIcon} from '../assets/icons/home.svg'
+import { ReactComponent as Close } from '../assets/icons/close.svg'
 import './Cart.css'
 import { removeFromCart } from '../redux/actions/cart'
-import { ReactComponent as Close } from '../assets/icons/close.svg';
+
 
 function Cart(props) {
+
     const {cartProducts,removeFromCartInjected} = props
+    const totalSum = (products) => {
+        return products.reduce((acc, product) => {
+            return acc + product.quantity * product.price
+        }, 0)
+    }
+
     return (
         <Layout>
             <div className='container-fluid container-min-max-width
@@ -23,28 +31,40 @@ function Cart(props) {
                     </div>
                    {cartProducts.map( (cartProduct) => {
                        return (
-                       <div className="d-flex justify-content-between align-items-center text-center my-2" key={cartProduct.id}>
+                       <div className="d-flex justify-content-between align-items-center text-center text-white my-2" key={cartProduct.id}>
                            <div className="w-25 d-flex flex-column justify-content-center align-items-center my-2">
                                 <img src={cartProduct.image} alt="product" />
-                                <p className='text-white m-0'>{cartProduct.name}</p>
+                                <p className='m-0'>{cartProduct.name}</p>
                            </div>
-                           <p className="w-25 text-white my-2">{ cartProduct.price } { cartProduct.currency }</p>
-                           <p className="w-25 text-white my-2">{ cartProduct.quantity }</p>
-                           <div className="w-25 d-flex justify-content-end text-white align-items-center my-2">
-                                <p className='cart-price d-flex justify-content-start m-0'>
-                                    { cartProduct.price * cartProduct.quantity } { cartProduct.currency } 
+                           <p className="w-25 my-2">{ cartProduct.price } { cartProduct.currency }</p>
+                           <p className="w-25 my-2">{ cartProduct.quantity }</p>
+                           <div className="w-25 d-flex justify-content-center align-items-center my-2">
+                                <p className='m-0'>
+                                    { cartProduct.price * cartProduct.quantity }{ cartProduct.currency } 
                                 </p>
                                 <div onClick={()=>{
                                         removeFromCartInjected ({
                                                 id: cartProduct.id
                                             })
                                     }}>
-                                            <Close className='close-svg d-flex align-items-center'/>
+                                            <Close className='close-svg'/>
                                 </div>
                            </div>
                        </div> )
                             
                    })}
+                   <div className="d-flex justify-content-end border-top mt-4 text-white">
+                            <div className="w-25 d-flex align-items-center justify-content-center">
+                                <Link to='/stripe'>
+                                    <button className="btn btn-outline-light p-2">Checkout</button>
+                                </Link>
+                            </div>
+                            <div className="w-25">
+                                <p className="my-4 p-1 h4 text-center">
+                                    { totalSum(cartProducts) }{ cartProducts[0].currency }
+                                </p>
+                            </div>
+                    </div>
                 </div>
                 : <div className='d-flex flex-column align-items-center justify-content-center'>
                     <p className='text-white my-5 lead'>No products in cart!</p>
@@ -55,7 +75,6 @@ function Cart(props) {
                         </button>
                     </Link>
                 </div>
-
             }
             </div>
         </Layout>
@@ -68,12 +87,11 @@ function mapStateToProps(state){
     }
 
 }
+
 function mapDispatchToProps(dispatch){
     return {
         removeFromCartInjected: (payload) => dispatch(removeFromCart(payload))
     }
 }
-
-
 
 export default connect(mapStateToProps,mapDispatchToProps)(Cart)
