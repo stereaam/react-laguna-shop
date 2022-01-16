9e49b989f6ad3b53d606614acf80da46c65834a4
stereaam <mihai.sterea@yahoo.com>
2022-01-16 18:29:25 +0200

    updated ProductItem and ProductList components

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,44 +1,115 @@
 import React from 'react'
 import './ProductItem.css'
 import { connect } from 'react-redux'
-import {addToCart} from '../redux/actions/cart'
+import { addToCart } from '../redux/actions/cart'
+import { addToFavorites, removeFromFavorites } from '../redux/actions/favorites'
+import { ReactComponent as EmptyHeart} from '../assets/icons/empty-heart.svg'
+import { ReactComponent as ColoredFullHeart} from '../assets/icons/colored-full-heart.svg'
+import { ReactComponent as CloseRed} from '../assets/icons/close-red.svg'
+import { Link } from 'react-router-dom'
 
 
 
 function ProductItem(props) {
     
-    const {id,name,price,currency,image,addToCartInjected} = props
-
+    const {id,name,price,currency,image,addToCart,addToFavorites, removeFromFavorites, favoriteProducts} = props
+    const productInCart = favoriteProducts.find(product => product.id===id)
+    
     return (
-        <div className="product-item col-sm-12 col-md-4 d-flex flex-column align-items-center">
-            <img src={image} alt="productPhoto" className="category-image my-1 category-image"/>
-            <p className="m-2 text-center text-white">{ name }</p>
-            <p className="mb-3 text-center text-white">{ price + currency }</p>
-            <button 
-                className='btn btn-outline-light mb-3'
-                onClick={()=>{
-                        addToCartInjected({
-                            product: {
-                                id,
-                                name,
-                                price,
-                                currency,
-                                image
-                            }
-                        })
+        <div className="col-sm-12 col-md-4 d-flex flex-column align-items-center p-0">
+
+            <Link to={`/product/${id}`}>
+                <div className='d-flex'>
+                    <img src={image} alt="productPhoto" className="category-image"/>
+                </div>
+
+            </Link>
+
+            <Link to={`/product/${id}`}>
+                <p className="my-1 mb-0 text-center text-white">{ name }</p>
+            </Link>
+
+            <Link to={`/product/${id}`}>
+                <p className="mb-1 text-center text-white">{ price + currency }</p>
+            </Link>
+            
+            <div>
+                <button 
+                    className='btn btn-outline-light mb-5 mx-3'
+                    onClick={()=>{
+                            addToCart({
+                                product: {
+                                    id,
+                                    name,
+                                    price,
+                                    currency,
+                                    image
+                                }
+                            })
+                        }
                     }
+                >
+                    Add to cart
+                </button>
+
+                {props.url !=="/favorites"
+                    ?(productInCart
+                        ?<ColoredFullHeart
+                            className='pointer mb-5'
+                            onClick={()=>{
+                                removeFromFavorites({
+                                    product: {id}
+                                    })
+                                }
+                            }
+                        />
+                        :<EmptyHeart
+                            className='pointer mb-5'
+                            onClick={()=>{
+                                    addToFavorites({
+                                        product: {
+                                            id,
+                                            name,
+                                            price,
+                                            currency,
+                                            image
+                                        }
+                                    })
+                                }
+                            }
+                        />
+                    )
+
+                    :<CloseRed
+                        className='pointer mb-5'
+                        onClick={()=>{
+                            removeFromFavorites({
+                                product: {id}
+                                })
+                            }
+                        }
+                    />
                 }
-            >
-                Add to cart
-            </button>
+
+            </div>
+       
         </div>
     )
 }
 
-function mapDispatchToProps(dispatch){
+
+function mapStateToProps(state){
     return {
-        addToCartInjected: (payload) => dispatch(addToCart(payload))
+        favoriteProducts: state.favorites.products
     }
 }
 
-export default connect(null,mapDispatchToProps)(ProductItem)
+function mapDispatchToProps(dispatch){
+    return {
+        addToCart: (payload) => dispatch(addToCart(payload)),
+        addToFavorites: (payload) => dispatch(addToFavorites(payload)),
+        removeFromFavorites: (payload) => dispatch(removeFromFavorites(payload))
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(ProductItem)
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import ProductItem from './ProductItem'
 
 function ProductList(props) {
+    
     const {products} = props
     return (
 
-        <div className='row mb-3'>
+        <div className='row m-3 p-0'>
             {
                 products.map((product) =>{
                     return <ProductItem {...product} key={product.id} />
