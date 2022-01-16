a1b81ced8157f1f8049a8313937b6b59608644be
stereaam <mihai.sterea@yahoo.com>
2022-01-16 18:30:03 +0200

    added TermsAndConditions page

---
+++ src/pages/TermsAndConditions.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import Layout from '../components/Layout'
+import './TermsAndConditions.css'
+
+function TermsAndConditions() {
+    return (
+        <Layout>
+            <div className='container-fluid container-min-max-width
+                d-flex flex-column justify-content-center align-items-start text-justify text-white terms-and-conditions'>
+                <h1 className='py-2 mt-3'>Terms and conditions</h1>
+                <div>
+                    <p>
+                        Last updated: January 14, 2022
+                        Please read these terms and conditions carefully before using Our Service.
+                    </p> 
+
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, eos! Dolore est ut cumque magni fugit vitae voluptatum, tenetur iste! Necessitatibus laudantium nobis minus aut similique eos ullam unde adipisci reprehenderit dolorum, explicabo pariatur nam totam quaerat! Dicta, quis. Sit modi aliquam eum obcaecati deleniti, nemo quibusdam maxime labore autem? Corporis, dolorem nulla odit nemo doloremque nam accusamus recusandae modi eius voluptatem ratione possimus rem vero! Ullam reiciendis tempore consequuntur eos adipisci inventore odit necessitatibus amet officia nobis explicabo in veritatis modi similique et tenetur ab unde perspiciatis, quae dolores quibusdam veniam impedit qui provident? Porro qui consequatur incidunt officia?
+                    </p>
+
+                    <p> 
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem dolorem animi, cumque voluptas delectus quam reiciendis numquam quibusdam velit sint voluptatem, accusantium assumenda temporibus a. Nobis error magni ex accusamus, voluptate similique aut vitae reiciendis fugiat iusto qui exercitationem praesentium quam, laudantium eaque id voluptatum. Recusandae nam maiores vel id!
+                    </p>
+            
+                    <h3>Contact Us</h3> 
+                    <p>If you have any questions about these Terms and Conditions, You can contact us:</p> 
+                    <ul>
+                        <li>By email: mihai.sterea@yahoo.com</li>
+                        <li>By phone number: +40770159063</li>
+                    </ul>
+                </div>
+            </div>
+        </Layout>
+    )
+}
+
+export default TermsAndConditions
