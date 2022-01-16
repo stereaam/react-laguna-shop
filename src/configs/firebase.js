546f27ffea5e77810b416a863a4d06e44f99c61a
stereaam <mihai.sterea@yahoo.com>
2022-01-16 19:52:01 +0200

    firebase config

---
+++ src/configs/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBNQ2SJVUZYhBklv9I9dVWT092Yf4qaybQ",
+  authDomain: "react-laguna-shop.firebaseapp.com",
+  projectId: "react-laguna-shop",
+  storageBucket: "react-laguna-shop.appspot.com",
+  messagingSenderId: "534868212430",
+  appId: "1:534868212430:web:ea05e22133a8bf2e88ee9f",
+  measurementId: "G-6BJL9598WF"
+};
+
+export default firebaseConfig
