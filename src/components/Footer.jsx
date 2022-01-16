e7d44919354a8c516f24c4b86c34f5bd574bf9ca
stereaam <mihai.sterea@yahoo.com>
2022-01-16 18:44:34 +0200

    updated Footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import { ReactComponent as Phone } from '../assets/icons/phone.svg';
-import { ReactComponent as Mail } from '../assets/icons/mail.svg';
-import { ReactComponent as GitHub } from '../assets/icons/github.svg';
-import { ReactComponent as LinkedIn } from '../assets/icons/linkedin.svg';
+import { ReactComponent as Phone } from '../assets/icons/phone.svg'
+import { ReactComponent as Mail } from '../assets/icons/mail.svg'
+import { ReactComponent as GitHub } from '../assets/icons/github.svg'
+import { ReactComponent as LinkedIn } from '../assets/icons/linkedin.svg'
 import './Footer.css'
 
 function Footer() {
+    
     return (
         <footer>
             <div className="container-fluid container-min-max-width
                             d-flex flex-wrap justify-content-between">
-                <div>
-                    <h3 className="h5 mb-3">Some useful links:</h3>
+                <div className='m-1'>
+                    <h3 className="h5 my-2">Some useful links:</h3>
                     <p className="m-1">
                         <Link to='/about'>About</Link>
                     </p>
@@ -20,8 +21,8 @@ function Footer() {
                         <Link to='/terms-and-conditions'>Terms and conditions</Link>
                     </p>
                 </div>
-                <div>
-                    <h3 className="h5 mb-3">Contact:</h3>
+                <div className='m-1'>
+                    <h3 className="h5 my-2">Contact:</h3>
                     <p className="m-1">
                         <a href="mailto:mihai.sterea@yahoo.com">
                             <Mail className="footer-icon"/>
@@ -33,8 +34,8 @@ function Footer() {
                         <span className="mx-1">+40770159063</span>
                     </p>
                 </div>
-                <div>
-                    <h3 className="h5 mb-3">Contact:</h3>
+                <div className='m-1'>
+                    <h3 className="h5 my-2">Find me:</h3>
                     <p className="m-1">
                         <a href="https://github.com/stereaam"  target="_blank" rel='noreferrer'>
                             <GitHub className="footer-icon"/>
@@ -49,7 +50,7 @@ function Footer() {
                     </p>
                 </div>
             </div>
-            <div className="text-center py-3">
+            <div className="text-center py-1">
                 &copy; Andrei-Mihai Sterea, 2021
             </div>
         </footer>
