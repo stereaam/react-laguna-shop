166d93fd5b5e2844996b5c7bdad1e2dd9e0497a6
stereaam <mihai.sterea@yahoo.com>
2022-01-16 18:51:09 +0200

    add routes for new pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import './App.css';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import About from './pages/About';
-import Page404 from './pages/Page404';
-import Category from './pages/Category';
-import Cart from './pages/Cart';
-import {Route, Switch} from 'react-router-dom';
+import React from 'react'
+import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import About from './pages/About'
+import Page404 from './pages/Page404'
+import Category from './pages/Category'
+import Cart from './pages/Cart'
+import Product from './pages/Product'
+import Favorites from './pages/Favorites'
+import TermsAndConditions from './pages/TermsAndConditions'
+import Stripe from './pages/Stripe'
+import {Route, Switch} from 'react-router-dom'
 
 
 
@@ -25,6 +29,10 @@ class App extends React.Component{
           <Route path="/about" component={About} />
           <Route path="/category/:categoryName" component={Category} />
           <Route path="/cart" component={Cart} />
+          <Route path="/product/:productId" component={Product} />
+          <Route path="/favorites" component={Favorites} />
+          <Route path="/terms-and-conditions" component={TermsAndConditions} />
+          <Route path="/stripe" component={Stripe} />
           <Route path="*" component={Page404} />
         </Switch>
       </div>
