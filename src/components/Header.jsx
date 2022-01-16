3f6c28717d7d32e84696c6d7d5197efdd82bc003
stereaam <mihai.sterea@yahoo.com>
2022-01-16 18:50:11 +0200

    add user name to Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import Logo from '../assets/logo.png'
-import './Header.css'
-import { connect } from 'react-redux'
-import { ReactComponent as ShoppingCart } from '../assets/icons/shopping-cart.svg';
 import { signOutUser } from '../redux/actions/user'
-
+import { connect } from 'react-redux'
+import Logo from '../assets/logo.png'
+import { ReactComponent as ShoppingCart } from '../assets/icons/shopping-cart.svg'
+import { ReactComponent as FullHeart } from '../assets/icons/full-heart.svg'
+import './Header.css'
 
 function Header(props) {
-    const {numberOfProducts, userData, signOutUser} = props
+
+    const {numberOfCartProducts, numberOfFavoriteProducts, userData, signOutUser} = props
     return (
         <header className='header'>
             <div className='container-fluid container-min-max-width d-flex justify-content-between align-items-center'>
@@ -16,14 +17,24 @@ function Header(props) {
                 <Link to='/'><img src={Logo} alt="Laguna Shop" className='logo'/></Link>
                 <div>
                     { userData
-                    ? <p className='text-white lead m-0'>Hello, {userData}!</p>
+                    ? <div className='d-flex flex-column flex-sm-row justify-content-end align-items-end text-white'>
+                        <p className='p-0 m-0'>Hello,&nbsp;</p>
+                        <p className='p-0 m-0'>{userData}!</p>
+                      </div>
+                    
                     : null}
-                    <div className='d-flex align-items-center justify-content-end'>
-                        { userData
-                        ? <h2 className='logout-text' onClick={signOutUser}>Logout</h2>
-                        : <Link to='/login'><h2>Login</h2></Link>}
-                        <Link to='/cart'><ShoppingCart className='mx-2'/></Link>
-                        <p className='my-2 mr-1 number-of-products text-white'>{numberOfProducts}</p>
+                    <div className='d-flex flex-column flex-sm-row'>
+                        <div className='d-flex align-items-center justify-content-end'>
+                            { userData
+                            ? <h2 className='logout-text mx-2' onClick={signOutUser}>Logout</h2>
+                            : <Link to='/login'><h2 className='mx-2'>Login</h2></Link>}  
+                        </div>
+                        <div className='d-flex align-items-center justify-content-end'>
+                            <Link to='/favorites'><FullHeart className='mx-1 text-bold'/></Link>
+                            <p className='my-2 mr-1 number-of-products text-white'>{numberOfFavoriteProducts}</p>
+                            <Link to='/cart'><ShoppingCart className='mx-1'/></Link>
+                            <p className='my-2 mr-1 number-of-products text-white'>{numberOfCartProducts}</p>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -32,7 +43,8 @@ function Header(props) {
 }
 function mapStateToProps(state){
     return {
-        numberOfProducts: state.cart.products.length,
+        numberOfCartProducts: state.cart.products.length,
+        numberOfFavoriteProducts: state.favorites.products.length,
         userData : state.user.data
     }
 }
