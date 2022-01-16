708066127e76857915a9aa801a5b22a92ff700ba
stereaam <mihai.sterea@yahoo.com>
2022-01-16 18:48:29 +0200

    updated redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,14 @@
 import { createStore , combineReducers, applyMiddleware } from "redux";
 import cartReducer from './reducers/cart'
 import userReducer from "./reducers/user";
+import favoritesReducer from "./reducers/favorites"
 import thunk from 'redux-thunk'
 
 const rootReducer = combineReducers({
     cart: cartReducer,
-    user: userReducer}
-    )
+    user: userReducer,
+    favorites: favoritesReducer
+})
 
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
