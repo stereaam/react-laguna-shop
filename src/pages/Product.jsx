f598fe5031b570033086b9ccbc937830eab94ff8
stereaam <mihai.sterea@yahoo.com>
2022-01-19 17:48:09 +0200

    update Product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,7 +3,10 @@ import Layout from '../components/Layout'
 import products from '../utils/products.json'
 import './Product.css'
 import {connect} from 'react-redux'
-import { addToCart } from '../redux/actions/cart'
+import {addToCart} from '../redux/actions/cart'
+import {addToFavorites, removeFromFavorites} from '../redux/actions/favorites'
+import { ReactComponent as EmptyHeart} from '../assets/icons/empty-heart.svg'
+import { ReactComponent as ColoredFullHeart} from '../assets/icons/colored-full-heart.svg'
 
 class Product extends Component {
 
@@ -29,39 +32,69 @@ class Product extends Component {
 
     
 
-    render() {
+    render() {   
+        const productInCart = this.props.favoriteProducts.find(product => product.id === this.state.product.id)
+        const {product} = this.state
         return (
            <Layout>
                 <div className='product-page container-min-max-width container-fluid d-flex flex-column flex-wrap rounded'>
-                    <h1 className='product-name'>{this.state.product.name}</h1>
+                    <h1 className='product-name'>{product.name}</h1>
                     <div className='text-white d-flex align-items-center justify-content-center flex-wrap'>
                         <div className='d-flex flex-column align-items-center justify-content-center m-1'>
-                            <img className='m-2 py-3'  src={this.state.product.image} alt="product" />
+                            <img className='m-2 py-3'  src={product.image} alt="product" />
                         </div>
                         <div className='product-info rounded mb-3 mt-1 py-5 px-3 mx-3'>
-                            <p className='m-1 p-1'><strong>Price:</strong> {this.state.product.price}{this.state.product.currency}</p>
-                            <p className='m-1 p-1'><strong>Brand:</strong> {this.state.product.brand}</p>
-                            <p className='m-1 p-1'><strong>Size: </strong>{this.state.product.size}</p>
-                            <p className='m-1 p-1'><strong>Color: </strong>{this.state.product.colour}</p>
-                            <p className='m-1 p-1'><strong>Material: </strong>{this.state.product.material}</p>
-                            <p className='m-1 p-1'><strong>Description: </strong>{this.state.product.description}</p>
-                            <button 
-                                className='btn btn-outline-light m-1'
-                                onClick={()=>{
-                                        this.props.addToCart({
-                                            product: {
-                                                id : this.state.product.id,
-                                                name: this.state.product.name,
-                                                price: this.state.product.price,
-                                                currency: this.state.product.currency,
-                                                image:this.state.product.image
-                                            }
-                                        })
+                            <p className='m-1 p-1'><strong>Price:</strong> { product.price}{ product.currency}</p>
+                            <p className='m-1 p-1'><strong>Brand:</strong> { product.brand}</p>
+                            <p className='m-1 p-1'><strong>Size: </strong>{ product.size}</p>
+                            <p className='m-1 p-1'><strong>Color: </strong>{ product.colour}</p>
+                            <p className='m-1 p-1'><strong>Material: </strong>{ product.material}</p>
+                            <p className='m-1 p-1'><strong>Description: </strong>{ product.description}</p>
+                            <div>
+                                <button 
+                                    className='btn btn-outline-light m-1'
+                                    onClick={()=>{
+                                            this.props.addToCart({
+                                                product: {
+                                                    id :  product.id,
+                                                    name:  product.name,
+                                                    price:  product.price,
+                                                    currency:  product.currency,
+                                                    image: product.image
+                                                }
+                                            })
+                                        }
                                     }
-                                }
-                            >
-                                Add to cart
-                            </button>
+                                >
+                                    Add to cart
+                                </button>
+                                {productInCart
+                                ?<ColoredFullHeart
+                                    className='pointer m-1'
+                                    onClick={()=>{
+                                        this.props.removeFromFavorites({
+                                            product: {id: product.id}
+                                            })
+                                        }
+                                    }
+                                />
+                                :<EmptyHeart
+                                    className='pointer m-1'
+                                    onClick={()=>{
+                                            this.props.addToFavorites({
+                                                product: {
+                                                    id: product.id,
+                                                    name: product.name,
+                                                    price: product.price,
+                                                    currency: product.currency,
+                                                    image: product.image
+                                                }
+                                            })
+                                        }
+                                    }
+                                />
+                                }        
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -71,10 +104,19 @@ class Product extends Component {
 
     
 }
-let mapDispatchToProps = (dispatch) =>{
+
+function mapStateToProps(state){
+    return {
+        favoriteProducts: state.favorites.products
+    }
+}
+
+function mapDispatchToProps (dispatch){
         return{
-            addToCart: (payload) => dispatch(addToCart(payload))
+            addToCart: (payload) => dispatch(addToCart(payload)),
+            addToFavorites: (payload) => dispatch(addToFavorites(payload)),
+            removeFromFavorites: (payload) => dispatch(removeFromFavorites(payload))
         }
     }
 
-export default connect(null,mapDispatchToProps)(Product)
+export default connect(mapStateToProps,mapDispatchToProps)(Product)
